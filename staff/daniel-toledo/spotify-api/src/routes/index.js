b8a2d205e67cf1c5e7650415f04a10a4ce09ef52
danieltolmon <daniel.toledomonfort@gmail.com>
2019-02-22 20:20:12 +0100

    liada montada

---
+++ staff/daniel-toledo/spotify-api/src/routes/index.js
@@ -0,0 +1,25 @@
+module.exports = {
+    registerUser: require('./register-user'),
+
+    authenticateUser: require('./authenticate-user'),
+
+    retrieveUser: require('./retrieve-user'),
+
+    searchArtists: require('./search-artists'),
+
+    retriveArtist: require('./retrieve-artist'),
+
+    retriveAlbum: require('./retrieve-album'),
+
+    retireveTrack: require('./retrieve-track'),
+
+    // TODO other route handlers
+    toggleFavoriteArtist: require('./favorite-artist'),
+
+    listCommentsFromArtist: require('./list-comment-to-artist'),
+
+    addCommentsFromArtist: require('./add-comment-to-artist'),
+
+    notFound: require('./not-found'),
+
+}
